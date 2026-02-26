62c0b450907ae17281e9964bdab21d04288a3821
Kirk <ksahdo10@gmail.com>
2026-02-26 16:43:54 -0400

    fix: appPort number cast

--- src/shared/infrastructure/env-config/env-config.service.ts
+++ src/shared/infrastructure/env-config/env-config.service.ts
@@ -7,7 +7,7 @@ export class EnvConfigService implements IEnvConfig {
   constructor(private readonly configService: ConfigService) {}
 
   getAppPort(): number {
-    return this.configService.get<number>('APP_PORT', 3000);
+    return Number(this.configService.get<number>('APP_PORT', 3000));
   }
 
   getNodeEnv(): string {
