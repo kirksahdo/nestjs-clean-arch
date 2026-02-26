9d24c159aad56bb3b0e99cd1ef0f455cd3cb18d5
Kirk <ksahdo10@gmail.com>
2026-02-26 16:14:37 -0400

    feat: implement env config service

--- src/shared/infrastructure/env-config/env-config.service.ts
+++ src/shared/infrastructure/env-config/env-config.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { IEnvConfig } from './env-config.interface';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class EnvConfigService implements IEnvConfig {
+  constructor(private readonly configService: ConfigService) {}
+
   getAppPort(): number {
-    throw new Error('Method not implemented.');
+    return this.configService.get<number>('APP_PORT', 3000);
   }
+
   getNodeEnv(): string {
-    throw new Error('Method not implemented.');
+    return this.configService.get<string>('NODE_ENV', 'development');
   }
 }
